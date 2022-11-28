d823199c1541cb5e7647f3adc89bf112b6f004ef
Jaisoft <jaime.gomez.moraleda@gmail.com>
2022-11-28 18:00:24 +0100

    add docs

--- docker/docker-compose-cli/docker-compose/springboot-postgresql-docker-example/src/main/java/com/experto/springbootpostgresqldocker/repository/UserRespository.java
+++
@@ -1,9 +0,0 @@
-package com.experto.springbootpostgresqldocker.repository;
-
-import com.experto.springbootpostgresqldocker.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRespository extends JpaRepository<UserModel, Long> {
-}
